bc349e8446f089bb261aa205f480dcc84a327529
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-12 10:44:34 -0600

    changing build controls

--- burger/src/components/Burger/Burger.js
+++ burger/src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  return (<div className={classes.Burger}>
-    <BurgerIngredient type='bread-top'/>
-    <BurgerIngredient type='cheese'/>
-    <BurgerIngredient type='meat'/>
-    <BurgerIngredient type='bread-bottom'/>
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey}/>
+    })
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, [])
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start building!</p>
+  }
 
+  console.log(transformedIngredients)
+
+  return (<div className={classes.Burger}>
+    <BurgerIngredient type='bread-top'/> {transformedIngredients}
+    <BurgerIngredient type='bread-bottom'/>
   </div>)
 
 }
