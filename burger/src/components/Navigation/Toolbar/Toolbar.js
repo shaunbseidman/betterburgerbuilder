c6c794bb8e41f31d84440a578f1bf3005c82efc3
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-12 14:04:13 -0600

    added sidebar

--- burger/src/components/Navigation/Toolbar/Toolbar.js
+++ burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => {
   return(
     <header className = {classes.Toolbar}>
       <div>Menu</div>
-      <Logo />
-      <nav>...</nav>
+      <Logo height='80%' />
+      <NavigationItems />
     </header>
   )
 }
