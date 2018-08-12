bdb7c09dbb0e79e1f4079cf59613ec5d1e42a3f1
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-12 11:57:06 -0600

    disabled button for negative array

--- burger/src/components/Burger/BuildControls/BuildControls.js
+++ burger/src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,13 @@ const buildControls = (props) =>{
   return(
     <div className = {classes.BuildControls}>
       {controls.map(ctrl => (
-        <BuildControl key={ctrl.label} label={ctrl.lable} />
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled}
+          />
       ))}
     </div>
   )
